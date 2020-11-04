c035331194eee0835a23756e39b68444daf62bbc
Harun1302123 <ahmharunurrashid@gmail.com>
2020-11-04 16:34:20 +0600

    calculation in old school

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ var numbers = [1, 2, 3, 4, 5, 6];
 function sum(n) {
     var summation = 0;
     //
-    
+    for (let i = 0; i < n.length; i++) {
+        summation += n[i];
+    }
 
     return summation;
 }
