3d72ab649e23db7305f67e5621407746bbbc88a1
Harun <35065199+Harun1302123@users.noreply.github.com>
2020-11-04 18:08:45 +0600

    add comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var numbers = [1, 2, 3, 4, 5, 6];
 function sum(n) {
     var summation = 0;
-    //
+    //calcultation for summation
     for (let i = 0; i < n.length; i++) {
         summation += n[i];
     }
